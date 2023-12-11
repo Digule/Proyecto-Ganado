294c6d8eda36cb5653d89b511e359939c004f00b
Juan <elranchodejuancho12@gmail.com>
2023-12-11 10:08:34 +0100

    le he añadido código a la vaca

---
+++ Vaca.java
@@ -0,0 +1,11 @@
+public class Vaca {
+    private String nombre;
+    private int peso;
+    private Leche lech_vaca;
+    
+    public Vaca (String nom) {
+        nombre = nom;
+        peso = 100;
+        lech_vaca = new Leche(false, 0);
+    }
+}
