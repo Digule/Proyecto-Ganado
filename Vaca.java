0170ade4aacfe20b78d4b4310392b3d1967b02c4
Diego <digule@gmail.com>
2023-12-19 11:10:46 +0100

    Commit 5

--- Vaca.java
+++ Vaca.java
@@ -2,4 +2,10 @@ public class Vaca {
     private String nombre;
     private int peso;
     private Leche lech_vaca;
+    
+    public Vaca (String nombre) {
+        this.nombre = nombre;
+        peso = 100;
+        lech_vaca = new Leche(false, 0);
+    }
 }
